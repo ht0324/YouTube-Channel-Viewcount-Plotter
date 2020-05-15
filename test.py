2e36efe52ed457f4b4f41f8cca6cd9c13a256df8
ht0324 <49386658+ht0324@users.noreply.github.com>
2020-05-15 21:10:00 +0900

    Add files via upload
    refactoring code using function

---
+++ test.py
@@ -0,0 +1,56 @@
+import json
+import re
+import urllib.request
+import matplotlib.pyplot as plt
+import csv
+
+# from pytube import YouTube
+
+api_key = "AIzaSyCFz_mD6HVPoaICeveS-SrIxrBqJ98kslo"
+channelname=None
+maxResults=None
+def getUploadsPlaylist():
+    url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername={channelname}&key={api_key}"
+
+    json_url = urllib.request.urlopen(url)
+    data = json.loads(json_url.read())
+    uploads=data["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+    url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults={maxResults}&playlistId={uploads}&key={api_key}"
+
+#    json_url = urllib.request.urlopen(url)
+    return json.loads(urllib.request.urlopen(url).read())
+    
+def processData(data):
+    dataList=[]
+    for i in data["items"]:
+        video=i["snippet"]["resourceId"]["videoId"]
+        url =f"https://www.googleapis.com/youtube/v3/videos?id={video}&part=snippet,statistics&key={api_key}"
+        data = json.loads(urllib.request.urlopen(url).read())
+    #    print(data["items"][0]["statistics"])
+        views = int(data["items"][0]["statistics"]["viewCount"])
+        dataList.append(views)
+    return dataList
+
+def printGraph(dataList):
+    x=0
+    xlist=[]
+    for i in range(len(dataList)):
+        xlist.append(x)
+        x+=1
+    plt.plot(xlist,list(map(int,dataList)))
+    plt.xlabel("Video No.")
+    plt.ylabel("View count")
+    plt.title("YouTube Channel: "+channelname)
+    plt.show()
+    
+def main():
+    global channelname, maxResults
+    maxResults="50"
+    channelname="unboxtherapy"
+#    channelname=input()
+    data=getUploadsPlaylist()
+    dataList=processData(data)
+    printGraph(dataList)
+    
+if __name__ == "__main__":
+    main()
