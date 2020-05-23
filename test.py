211d004457e09437f411c5a202a9639699b5aa5d
ht0324 <49386658+ht0324@users.noreply.github.com>
2020-05-23 10:10:46 +0900

    implementing *args, putting video_data in class

--- test.py
+++ test.py
@@ -24,7 +24,6 @@ def get_videos(data):
         video_id = video["snippet"]["resourceId"]["videoId"]
         url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&part=snippet,statistics&key={api_key}"
         data = json.loads(urllib.request.urlopen(url).read())
-#        print(data["items"][0]["statistics"])
         video_id_list.append(data)
     return video_id_list
     
@@ -35,30 +34,52 @@ def get_data(video_id_list,type):
         data_list.append(views)
     return data_list
 
-def print_graph(data_list):
+class video_data:
+    def __init__(self, video_id_list, type):
+        self.type=type
+        self.video_id_list=video_id_list
+        self.data_list = []
+        for video in video_id_list:
+            views = int(video["items"][0]["statistics"][type])
+            self.data_list.append(views)
+            
+    def print_graph(self):
+        x = 0
+        xlist = []
+        for video in range(len(self.data_list)):
+            xlist.append(x)
+            x += 1
+        plt.plot(xlist,list(map(int,self.data_list)),label=self.type)
+        plt.xlabel("video_id No.")
+        plt.ylabel("View count")
+        plt.title("YouTube Channel: " + channel_name)
+        plt.show()
+
+def print_graph(*argv):
     x = 0
     xlist = []
-    for video in range(len(data_list)):
+    for video in range(max_results):
         xlist.append(x)
         x += 1
-    plt.plot(xlist,list(map(int,data_list)))
+    for args in argv:
+        plt.plot(xlist,list(map(int,args.data_list)),label=args.type)
     plt.xlabel("video_id No.")
     plt.ylabel("View count")
     plt.title("YouTube Channel: " + channel_name)
     plt.show()
-    
+
 def main():
     global channel_name, max_results
-    max_results = 50
+    max_results = 25
     channel_name = "unboxtherapy"
-#    channel_name=input()
     data = get_uploads_playlist()
     video_id_list=get_videos(data)
-    view = get_data(video_id_list,"viewCount")
-#    like = get_data(video_id_list,"likeCount")
-#    dislike = get_data(video_id_list,"dislikeCount")
-#    comment = get_data(video_id_list,"commentCount")
-    print_graph(view)
+    view=video_data(video_id_list,"viewCount")
+    like=video_data(video_id_list,"likeCount")
+    dislike=video_data(video_id_list,"dislikeCount")
+    comment=video_data(video_id_list,"commentCount")
+    print_graph(view, like, dislike, comment)
+
     
 if __name__ == "__main__":
     main()
